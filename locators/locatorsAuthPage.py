b86841636ec4f4750a4d9860044ae4c0e2b294a7
Elya-autho <filippova.elvina2@gmail.com>
2025-12-01 15:22:46 +0500

    add file

---
+++ locators/locatorsAuthPage.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+class Login_page_locators:
+    INPUT_PHONE = (By.XPATH, '//*[@class="login-phone-number"]')
+    SEND_BUTTON_NEXT = (By.XPATH, '//*[@class="login-phone-next"]')
+    INPUT_SMS_CODE = (By.XPATH, '//*[@id="app"]/div/input')
+    SEND_BUTTON_NEXT_SMS_CODE = (By.XPATH, '//*[@class="login-sms-code"]')
+    CLICK_LINK_SMS_CODE = (By.XPATH, '//*[@class ="login-sms-code-again-a"]')
--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,11 +1,28 @@
 from selenium import webdriver
 import time
+from locators.locatorsAuthPage import Login_page_locators
 from selenium.webdriver.common.by import By
 
 URL = "https://test.kb-monita.ru/"
+PHONE = 9085728301
+
+#Открываем страницу
+# browser = webdriver.Chrome()
+# browser.get(URL)
+# time.sleep(5)
+#
+# browser.quit()
+
+#Вводим данные для авторизации
 
 browser = webdriver.Chrome()
 browser.get(URL)
+time.sleep(5)
+input_phone = browser.find_element(*Login_page_locators.INPUT_PHONE).send_keys(PHONE)
 time.sleep(2)
+send_next =  browser.find_element(*Login_page_locators.SEND_BUTTON_NEXT).click()
+time.sleep(2)
+
+browser.quit()
 
 
