79330a6476b7a609c98e0fdd77af01a74c570581
Elya-autho <filippova.elvina2@gmail.com>
2026-05-15 17:59:54 +0500

    add file 16.03

--- tests/pi/conftest_py.py
+++ tests/pi/conftest_py.py
@@ -6,17 +6,6 @@ from base.urls import pi_rec_rules
 fake = Faker('ru')
 
 
-
-
-@pytest.fixture
-def browser(base_url):
-
-    driver = webdriver.Chrome()
-    driver.get(base_url)
-    yield driver
-
-    driver.quit()
-
 @pytest.fixture
 def url_rec_rules():
     return 'https://test.kb-monita.ru/#rec_rules'
--- tests/pi/rec_rules.py
+++ tests/pi/rec_rules.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 import time
+import pytest
 from locators.locatorsPiRecRul import PiRecRulesPage, PiRecRulesCreate
 from base.db import get_code
 from selenium.webdriver.common.by import By
+from  conftest import  browser, authorized_browser, base_url, user_phone
+from conftest_py import url_rec_rules
 
 
-def test_open_page_rec_rules(browser, url_rec_rules):
 
-
+def test_open_page_rec_rules(browser, authorized_browser,user_phone, url_rec_rules):
+    browser.get(url_rec_rules)
+    time.sleep(5)
+    assert "#rec_rules" in browser.current_url
